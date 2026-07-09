b86ad2fcf057cfba55e58880277fdbc6630f1c7b
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-07-09 22:48:35 +0200

    qoshimchalar

--- arifmetik_ammalar_sinteks.py
+++ arifmetik_ammalar_sinteks.py
@@ -1,8 +1,12 @@
 # 03 dars print funksiyalar
 # 2026.07 09
 print("Hello, World!")
-print("Assalomu aleykum, Dasturchilar!"
+print("Assalomu aleykum, Dasturchilar!")
 print("Hayrli tong!")
+print('Men "Dell" markasidagi noutbok sotib oldim')
+print('Men "Dell" markasidagi noutbok sotib oldim')
+print("Men \"Dell\" markasidagi noutbok sotib oldim")
+
 
 
 
